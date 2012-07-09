1c38ea07c02f377dfb563a48575d07c83f93030a
Miguel Peixe <miguel@cardume.art.br>
2012-07-09 10:07:38 -0300

    testing disable jscrollpane for safari

--- js/site.js
+++ js/site.js
@@ -1,5 +1,25 @@
 /* SITE SETUP */
 
+//distinguish chrome from safari in jquery
+var userAgent = navigator.userAgent.toLowerCase(); 
+$.browser.chrome = /chrome/.test(navigator.userAgent.toLowerCase()); 
+
+// Is this a version of Chrome?
+if($.browser.chrome){
+  userAgent = userAgent.substring(userAgent.indexOf('chrome/') +7);
+  userAgent = userAgent.substring(0,userAgent.indexOf('.'));
+  $.browser.version = userAgent;
+  // If it is chrome then jQuery thinks it's safari so we have to tell it it isn't
+  $.browser.safari = false;
+}
+
+// Is this a version of Safari?
+if($.browser.safari){
+  userAgent = userAgent.substring(userAgent.indexOf('safari/') +7);
+  userAgent = userAgent.substring(0,userAgent.indexOf('.'));
+  $.browser.version = userAgent;
+}
+
 /* CAROUSEL */
 
 $(document).ready(function() {
