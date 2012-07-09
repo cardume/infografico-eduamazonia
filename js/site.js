6c0a6b9b598eca94dabf02f19a7d0b44546b6ece
Miguel Peixe <miguel@cardume.art.br>
2012-07-08 23:58:54 -0300

    more about programas at visao geral

--- js/site.js
+++ js/site.js
@@ -7,6 +7,19 @@ $(document).ready(function() {
         slideSpeed: 700,
     });
     $('#carousel .reportagem').jScrollPane({autoReinitialise: true});
+
+    $('.programas-toggle').click(function() {
+        if($('#programas-more').hasClass('active')) {
+            $('#programas-more').removeClass('active');
+            $('#carousel .programas .graph').css({'visibility':'visible'});
+            $(this).text('Saiba mais sobre os programas');
+        } else {
+            $('#programas-more').addClass('active').jScrollPane();
+            $('#carousel .programas .graph').css({'visibility':'hidden'});
+            $(this).text('Ocultar informações sobre os programas');
+        }
+        return false;
+    });
 });
 
 
